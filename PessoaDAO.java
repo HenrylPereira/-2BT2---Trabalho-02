ea807e6b381032b55f9f0d0b8281a87ad373983d
Henry Lopes Pereira <73833452+HenrylPereira@users.noreply.github.com>
2022-06-21 21:42:30 -0300

    Update PessoaDAO.java

--- PessoaDAO.java
+++ PessoaDAO.java
@@ -9,19 +9,10 @@ public class PessoaDAO{
         return pessoas.get(id);
     }
 
-    /*
     public List<Pessoa> getAll(){
         ArrayList<Pessoa>lista = new ArrayList<>();
         lista.addAll(pessoas.values());
         return lista;
-    }
-
-    Tentei usar o getAll de forma tipada mas na hora da saida ele buga.
-
-    */
-
-    public HashMap getAll(){
-        return pessoas;
     }
 
     public void add(Pessoa pessoa){
